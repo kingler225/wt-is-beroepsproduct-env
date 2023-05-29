db08961fa43880e8cf575107fdd15842a92836b9
kingler225 <ri.koffijberg@student.han.nl>
2023-05-29 09:39:09 +0200

    Update hoelangnog.php

--- applicatie/hoelangnog.php
+++ applicatie/hoelangnog.php
@@ -4,11 +4,37 @@ $naam = 'Inger Koffijberg';
 $vandaag = date_create('now');
 $datum = $vandaag->format('d-M-Y');
 
-$sinterklaas = date_create('5-12-2023');
-$aantaldagen = date_diff($vandaag, $sinterklaas);
-$strAantalDagen = $aantaldagen->format('Y-m-d');
+$sinterklaas = DateTime::createFromFormat('d/m/Y', '05/12/2023');
+$aantalDagen = date_diff($vandaag, $sinterklaas);
+$strAantalDagen = $aantalDagen->format('%d');
 echo $strAantalDagen;
 
+$eenTMTien = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$zesTMVijftien = [11, 12, 13, 14, 15];
+$result = array_merge($eenTMTien, $zesTMVijftien);
+var_dump($result);
+
+
+$menu = [
+    [
+        'Eten' => [
+            'Shoarma' => 6.95,
+            'Appels' => 10.95,
+            'Tabouleh' => 8.95,
+            'Hamburger' => 5.50
+        ]
+        ],
+    [
+        'Drinken' => [
+            'Cola' => 2.00,
+            'Ayran' => 2.30,
+            'Fernandes' => 2.50,
+            'Bier' => 5.50
+        ]
+    ]
+        ];
+
+var_dump($menu);
 ?>
 <!DOCTYPE html>
 <html lang="nl">
@@ -19,6 +45,6 @@ echo $strAantalDagen;
 <body>
     Hallo <?= $naam ?>.<br>
     Het is vandaag <?= $datum ?>.
-    Het duurt nog <?=$strAantalDagen ?> tot Sinterklaas.
+    Het duurt nog <?=$strAantalDagen ?> dagen tot Sinterklaas.
 </body>
 </html>
