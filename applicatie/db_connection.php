6983cef2526d5d1c9672acde5167946789fbe464
kingler225 <ri.koffijberg@student.han.nl>
2023-06-04 09:53:47 +0200

    Update db_connection.php

--- applicatie/db_connection.php
+++ applicatie/db_connection.php
@@ -1,5 +1,15 @@
 <?php
 
+$hostname = 'database_server';
+$dbname = 'GelreAirport';
+$username = 'sa';
+$password = 'abc123!@#';
+
+$connection = new PDO('sqlsrv:Server='. $hostname .';Database='. $dbname .';ConnectionPooling=0;TrustServerCertificate=1', $username, $password);
+
+
+$passengers = $connection->query('SELECT * FROM Passagier');
+print_r($passengers);
 echo "hello world";
 
 ?>
